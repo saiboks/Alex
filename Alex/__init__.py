2524c25d000d261bb1aeefc1d1cf51fa509d1893
saiboks <botssai69@gmail.com>
2024-10-31 09:54:35 +0530

    Update __init__.py

--- Alex/__init__.py
+++ Alex/__init__.py
@@ -1,1 +1,26 @@
+from Alex.core.bot import Anony
+from Alex.core.dir import dirr
+from Alex.core.git import git
+from Alex.core.userbot import Userbot
+from Alex.misc import dbb, heroku
 
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Anony()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
