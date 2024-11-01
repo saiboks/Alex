403ef0c778e0d9912320d45019bbbb8434831738
saiboks <botssai69@gmail.com>
2024-11-01 13:19:42 +0530

    Update database.py

--- Alex/utils/database.py
+++ Alex/utils/database.py
@@ -733,7 +733,7 @@ async def get_filters_names(chat_id: int) -> List[str]:
     _filters = []
     for _filter in await _get_filters(chat_id):
         _filters.append(_filter)
-    return _filtersc import userbot
+        return _filtersc import userbot
 from Alex.core.mongo import mongodb
 
 authdb = mongodb.adminauth
