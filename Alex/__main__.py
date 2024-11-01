86bad50ab3416fb9a6f0b7c652f59d1fcba18a4e
saiboks <botssai69@gmail.com>
2024-11-01 14:08:49 +0530

    Update __main__.py

--- Alex/__main__.py
+++ Alex/__main__.py
@@ -1,14 +1,13 @@
 import asyncio
 import importlib
-
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from Alex import LOGGER, app, userbot
+from Alex import LOGGER  # Import only LOGGER globally
 from Alex.core.call import Anony
 from Alex.misc import sudo
-from Alexc.plugins import ALL_MODULES
+from Alex.plugins import ALL_MODULES
 from Alex.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
@@ -23,7 +22,9 @@ async def init():
     ):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
+
     await sudo()
+
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -31,27 +32,39 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
+    except Exception as e:
+        LOGGER(__name__).error(f"Error loading banned users: {e}")
         pass
+
+    # Import app and userbot locally to avoid circular import issues
+    from Alex import app, userbot
+
     await app.start()
+
     for all_module in ALL_MODULES:
-        importlib.import_module("Alex.plugins" + all_module)
+        importlib.import_module("Alex.plugins." + all_module)
+
     LOGGER("Alex.plugins").info("Successfully Imported Modules...")
+
     await userbot.start()
     await Anony.start()
+
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("Alex").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
         )
         exit()
-    except:
+    except Exception as e:
+        LOGGER(__name__).error(f"Error in starting stream call: {e}")
         pass
+
     await Anony.decorators()
     LOGGER("Alex").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
+
     await idle()
     await app.stop()
     await userbot.stop()
