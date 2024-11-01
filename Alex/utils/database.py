282ce3d6550113bcdd6cc16749ea974802c3275f
saiboks <botssai69@gmail.com>
2024-11-01 13:16:09 +0530

    Update database.py

--- Alex/utils/database.py
+++ Alex/utils/database.py
@@ -799,7 +799,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from Alex.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -813,7 +813,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AnonXMusic.core.userbot import assistants
+    from Alex.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -840,7 +840,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from Alex.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -853,7 +853,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AnonXMusic.core.userbot import assistants
+    from Alex.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
